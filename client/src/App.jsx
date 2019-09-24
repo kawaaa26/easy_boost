75dd1285a410dc33bda6895fa599f7b9410fc3d6
kawaaa26 <unic.almj@yahoo.com>
2019-09-24 21:46:44 +0900

    :bug: a line eliminated from App.jsx due to averting warnings.
    a line eliminated from App.jsx due to averting warnings. logo deleted also.
    
    Closes #18

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import MainContainer from './Components/MainContainer';
 import './App.css';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
