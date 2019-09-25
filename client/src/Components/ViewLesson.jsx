5e6520e26024027459f35b4fe6a7c4db653b7a9a
kawaaa26 <unic.almj@yahoo.com>
2019-09-25 20:16:29 +0900

    :art: Components/ViewLesson.jsx modified to show lesson details.
    Now the lesson details are properly shown.
    
    Closes #24

--- client/src/Components/ViewLesson.jsx
+++ client/src/Components/ViewLesson.jsx
@@ -5,7 +5,34 @@ export default class ViewLesson extends React.Component {
   render() {
     return (
       <div>
-        <span>{ this.props.data.lesson }</span>
+        <table>
+          <tbody>
+            <tr>
+              <td>NAME</td>
+              <td>STUDIO</td>
+              <td>PERFORMER</td>
+              <td>BAG NUMBER</td>
+              <td>CANCELLATION</td>
+            </tr>
+            <tr>
+              <td>
+                { this.props.data.name }
+              </td>
+              <td>
+                { this.props.data.studio }
+              </td>
+              <td>
+                { this.props.data.performer }
+              </td>
+              <td>
+                { this.props.data.bag_number }
+              </td>
+              <td>
+                { this.props.data.cancellation }
+              </td>
+            </tr>
+        </tbody>
+        </table>
       </div>
     )
   }
