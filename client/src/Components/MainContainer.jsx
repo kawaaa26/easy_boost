f6bbcdf7ae4133be331aec636467aaaa658205c1
kawaaa26 <unic.almj@yahoo.com>
2019-09-30 22:03:25 +0900

    :sparkles: Lesson Delete functionalities implemented.
    Closes #35

--- client/src/Components/MainContainer.jsx
+++ client/src/Components/MainContainer.jsx
@@ -27,8 +27,21 @@ export default class MainContainer extends React.Component {
     console.log(lesson)
     axios.post('http://localhost:3001/lessons', { lesson: lesson } )
       .then((response) => {
-        const newData = update(this.state.lessons, { $push:[response.data]})
-        this.setState({lessons: newData})
+        const newData = update(this.state.lessons, { $push:[response.data] })
+        this.setState({ lessons: newData })
+      })
+      .catch((data) => {
+        console.log(data)
+      })
+  }
+
+  deleteLesson = (id) => {
+    axios.delete(`http://localhost:3001/lessons/${id}`)
+      .then((response) => {
+        const lessonIndex = this.state.lessons.findIndex(x => x.id === id)
+        const deletedLessons = update(this.state.lessons, { $splice: [[lessonIndex, 1]] })
+        this.setState({ lessons: deletedLessons })
+        console.log('set')
       })
       .catch((data) => {
         console.log(data)
@@ -38,8 +51,8 @@ export default class MainContainer extends React.Component {
   render() {
     return (
       <div className="app-main">
-        <FormContainer createLesson={ this.createLesson } />
-        <LessonsContainer lessonData={ this.state.lessons } />
+        <FormContainer handleAdd={ this.handleAdd } createLesson={ this.createLesson } />
+        <LessonsContainer lessonData={ this.state.lessons } deleteLesson={ this.deleteLesson } />
       </div>
     );
   }
